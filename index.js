f61f0876a7a5be1ccdec183e297cb3dfde67c05a
MD Fayeakuzzaman <35700758+Fayeakuzzamantonmoy@users.noreply.github.com>
2021-10-28 01:07:10 +0600

    connect to git

---
+++ index.js
@@ -0,0 +1,50 @@
+const express = require('express');
+const { MongoClient } = require('mongodb');
+require('dotenv').config(); 
+const cors = require('cors');
+const app = express();
+const port = process.env.PORT || 5000;
+
+app.use(cors());
+app.use(express.json());
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.iut4d.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+
+
+
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+
+async function run(){
+    try{
+        await client.connect();
+        const database = client.db('carMechanic');
+        const servicesCollection = database.collection('services');
+
+        app.get('/services', async(req,res) => {
+            const cursor = servicesCollection.find({});
+            const services = await cursor.toArray();
+            res.send(services);
+        })
+
+        app.post('/services', async(req,res)=> {
+            const service = req.body;
+            console.log('hit the post api', service);
+            const result = await servicesCollection.insertOne(service);
+            console.log(result);
+            res.json(result)
+        });
+    }
+    finally{
+        //await client.close();
+    }
+}
+
+run().catch(console.dir);
+
+app.get('/', (req,res)=> {
+    res.send('Running Genius Server');
+});
+
+app.listen(port, ()=> {
+    console.log('Running Genius Server on port', port);
+})
